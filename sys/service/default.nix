524f5a9b16f80816c7bd62e1c7c175e76343cc98
hex <hex.execute@gmail.com>
2024-06-14 23:29:57 -0400

    added new services to imports

--- sys/service/default.nix
+++ sys/service/default.nix
@@ -1,5 +1,8 @@
 { self, ... }: {
   imports = [
+    "${self}/sys/service/gnome.nix"
+    "${self}/sys/service/polkit.nix"
+    "${self}/sys/service/upower.nix"
     "${self}/sys/service/bluetooth.nix"
     "${self}/sys/service/pipewire.nix"
   ];
