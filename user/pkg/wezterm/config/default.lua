27cb0e64add463571c6757b5e5c8a0530b115e89
hex <hex.execute@gmail.com>
2024-06-15 01:21:06 -0400

    added transparency and theme to wezterm

--- user/pkg/wezterm/config/default.lua
+++ user/pkg/wezterm/config/default.lua
@@ -2,7 +2,8 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'CatppuccinMocha'
+config.color_scheme = 'Catppuccin Mocha'
 config.enable_tab_bar = false
+config.window_background_opacity = 0.8
 
 return config
