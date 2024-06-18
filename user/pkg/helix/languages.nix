b7f52e01d4aa4f2f359f8edffa509c648c233a23
= <hex.execute@gmail.com>
2024-06-18 18:50:11 -0400

    added typescript and javascript lsp

--- user/pkg/helix/languages.nix
+++ user/pkg/helix/languages.nix
@@ -1,7 +1,17 @@
 { pkgs, ... }: {
-  programs.helix.languages.language = [{
-    name = "nix";
-    auto-format = true;
-    formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-  }];
+  programs.helix.languages.language = [
+    {
+      name = "nix";
+      auto-format = true;
+      formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+    }
+    {
+      name = "javascript";
+      auto-format = true;
+    }
+    {
+      name = "typescript";
+      auto-format = true;
+    }
+  ];
 }
