63821a2b4f44119e0f1b241dff9c492db0af6b66
= <hex.execute@gmail.com>
2024-06-19 22:06:47 -0400

    added findex

--- assets/ags/main.ts
+++ assets/ags/main.ts
@@ -21,6 +21,39 @@
 //   }),
 // });
 
+const top_bar = (monitor: number) => Widget.Window({
+  monitor,
+  name: `top_bar_${monitor}`,
+  anchor: ['top', 'left', 'right'],
+  exclusivity: 'exclusive',
+  child: Widget.CenterBox({
+    start_widget: Widget.Label({
+      hpack: 'center',
+      label: 'test'
+    })
+  })
+});
+
+Utils.monitorFile(
+  // directory that contains the scss files
+  `${App.configDir}/style`,
+
+  // reload function
+  () => {
+    // main scss file
+    const scss = `${App.configDir}/style/style.scss`;
+
+    // target css file
+    const css = `/tmp/ags-style.css`;
+
+    // compile, reset, apply
+    Utils.exec(`sass ${scss} ${css}`);
+    App.resetCss();
+    App.applyCss(css);
+  },
+);
+
 App.config({
-  windows: [],
+  configDir: "~/.dotfiles/assets/ags",
+  windows: [top_bar(0)],
 });
--- assets/ags/service/wallpaper.ts
+++ assets/ags/service/wallpaper.ts
@@ -1,5 +1,1 @@
-const WALLPAPER_FILE = `${Utils.HOME}/.dotfiles/assets/wallpaper/radio_antenna.png`
 
-class Wallpaper extends Service { }
-
-export default new Wallpaper
