497bf924bcd7aa511e947231e71df71fcb34212e
= <hex.execute@gmail.com>
2024-06-26 01:04:52 -0400

    attempt to fix options

--- common/theme/colors.nix
+++ common/theme/colors.nix
@@ -1,5 +1,10 @@
-{ self, ... }:
+{ self, pkgs, ... }:
 let
   colorsJSON = builtins.readFile "${self}/assets/material3/radio.json";
   colors = builtins.fromJSON colorsJSON;
-in { options.theme.colors = colors; }
+in {
+  options.theme.colors = pkgs.lib.mkOption {
+    default = colors;
+    description = "The color theme used by the system (material3).";
+  };
+}
