ce2d8de3998b08f5a594e3fe9009766fc7f25083
= <hex.execute@gmail.com>
2024-06-26 01:12:00 -0400

    attempt to fix nushell config path

--- user/shell.nix
+++ user/shell.nix
@@ -1,7 +1,7 @@
 {
   programs.nushell = {
     enable = true;
-    configFile.source = ./.../config.nu;
+    configFile.source = "~/.config/nushell/config.nu";
     extraConfig = ''
       let carapace_completer = {|spans|
       carapace $spans.0 nushell $spans | from json
