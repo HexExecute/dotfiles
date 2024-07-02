f51ea5d9d251646a6f6d6fa24a07830472490312
= <hex.execute@gmail.com>
2024-07-02 13:15:25 -0400

    added fenix

--- flake.nix
+++ flake.nix
@@ -3,23 +3,43 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, fenix, ... }@inputs:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
+      package.x86_64-linux.default =
+        fenix.packages.x86_64-linux.minimal.toolchain;
       nixosConfigurations.nixos = lib.nixosSystem {
         inherit system;
         specialArgs = { inherit inputs self; };
-        modules = [ ./sys/default.nix ];
+        modules = [
+          ./sys/default.nix
+          ({ pkgs, ... }: {
+            nixpkgs.overlays = [ pkgs.fenix.overlays.default ];
+            environment.systemPackages = [
+              (pkgs.fenix.complete.withComponents [
+                "cargo"
+                "clippy"
+                "rust-src"
+                "rustc"
+                "rustfmt"
+              ])
+              pkgs.rust-analyzer-nightly
+            ];
+          })
+        ];
       };
       homeConfigurations = {
         hex = home-manager.lib.homeManagerConfiguration {
@@ -28,18 +48,6 @@
 
           modules = [ ./user/default.nix ];
         };
-      };
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          pkgs.cargo
-          pkgs.rustc
-
-          pkgs.rust-analyzer
-          pkgs.rustfmt
-
-          pkgs.pkg-config
-          pkgs.openssl
-        ];
       };
     };
 }
