9a6c33da6bce923908a838c028e71a66494b77fe
= <hex.execute@gmail.com>
2024-07-02 23:02:22 -0400

    fixed brave pixelation

--- user/pkg/brave.nix
+++ user/pkg/brave.nix
@@ -1,20 +1,4 @@
-{ pkgs, lib, ... }:
-let
-  wrappedBrave = pkgs.buildEnv {
-    name = "wrappedBrave-${pkgs.brave.version}";
-    paths = [
-      pkgs.brave
-      (lib.hiPrio (pkgs.stdenv.mkDerivation {
-        name = "brave-wrap-brave";
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-        phases = [ "installPhase" ];
-        installPhase = ''
-          mkdir -p $out/bin
-          makeWrapper ${
-            lib.getExe pkgs.brave
-          } $out/bin/brave --add-flags --ozone-platform=wayland
-        '';
-      }))
-    ];
-  };
-in { home.packages = [ wrappedBrave ]; }
+{ pkgs, ... }: {
+  home.packages = with pkgs; [ brave ];
+  programs.chromium.commandLineArgs = [ "--ozone-platform=wayland" ];
+}
