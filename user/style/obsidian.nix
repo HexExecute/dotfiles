38621524290e905a677ffc6af75ec21a60538ded
= <hex.execute@gmail.com>
2024-07-15 18:29:39 -0400

    updated obsidian theme

--- user/style/obsidian.nix
+++ user/style/obsidian.nix
@@ -170,8 +170,9 @@
     body {
       --canvas-background: ${config.theme.colors.surface};
       --modal-background: ${config.theme.colors.surface};
-      --ribbon-background: ${config.theme.colors.surface};
+      --ribbon-background: ${config.theme.colors.surfaceContainer};
       --ribbon-background-collapsed: ${config.theme.colors.surfaceContainer};
+      --status-bar-background: ${config.theme.colors.surface};
       --adwaita-blue-1: #99c1f1;
       --adwaita-blue-2: #62a0ea;
       --adwaita-blue-3: #3584e4;
