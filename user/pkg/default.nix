6f0a371793763b35e42bdc9ec917d5100f8464ac
= <hex.execute@gmail.com>
2024-07-27 00:45:35 -0400

    added hid-tools

--- user/pkg/default.nix
+++ user/pkg/default.nix
@@ -13,6 +13,7 @@
     "${self}/user/pkg/gnome.nix"
     "${self}/user/pkg/gradience.nix"
     "${self}/user/pkg/helix"
+    "${self}/user/pkg/hid.nix"
     "${self}/user/pkg/javascript.nix"
     "${self}/user/pkg/kicad.nix"
     "${self}/user/pkg/krita.nix"
---
+++ user/pkg/hid.nix
@@ -0,0 +1,1 @@
+{ pkgs, ... }: { home.packages = with pkgs; [ hid-tools ]; }
