a8d283a1eb0823840d2413205295daee34d1762a
= <hex.execute@gmail.com>
2024-08-18 21:56:11 -0400

    added ani-cli

--- user/pkg/default.nix
+++ user/pkg/default.nix
@@ -1,6 +1,7 @@
 { self, ... }: {
   imports = [
     "${self}/user/pkg/alsa.nix"
+    "${self}/user/pkg/content.nix"
     "${self}/user/pkg/brave.nix"
     "${self}/user/pkg/btop.nix"
     "${self}/user/pkg/colorpicker.nix"
