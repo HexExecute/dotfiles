9f8c6a7750808eacfd419608388859da5c241fe0
= <hex.execute@gmail.com>
2024-09-14 12:44:43 -0400

    allow unfree packages fixed

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
-    unstablepkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    unstablepkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+      config.allowUnfree = true;
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
--- user/default.nix
+++ user/default.nix
@@ -11,7 +11,6 @@
   home.stateVersion = "24.05";
 
   nixpkgs.config.allowUnfree = true;
-  unstablepkgs.config.allowUnfree = true;
 
   programs.home-manager.enable = true;
 }
