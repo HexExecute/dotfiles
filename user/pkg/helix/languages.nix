76b757efd7cb382f8349ccdc3d14be6764ab4ccb
= <hex.execute@gmail.com>
2024-09-26 21:05:23 -0400

    fixed _ syntax error

--- user/pkg/helix/languages.nix
+++ user/pkg/helix/languages.nix
@@ -1,21 +1,21 @@
 { pkgs, ... }: {
   programs.helix.languages = {
-    language_server.texlab.config.texlab = {
+    language-server.texlab.config.texlab = {
       chktex = {
-        on_open_and_save = true;
-        on_edit = true;
+        on-open-and-save = true;
+        on-edit = true;
       };
       forward_search = {
         executable = "zathura";
         args = [ "--synctex-forward" "%l:%c:%f" "%p" ];
       };
       build = {
-        aux_directory = "build";
-        log_directory = "build";
-        pdf_directory = "build";
+        aux-directory = "build";
+        log-directory = "build";
+        pdf-directory = "build";
 
-        forward_search_after = true;
-        on_save = true;
+        forward-search-after = true;
+        on-save = true;
 
         executable = "tectonic";
 
@@ -34,24 +34,24 @@
     language = [
       {
         name = "nix";
-        auto_format = true;
+        auto-format = true;
         formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
       }
       {
         name = "javascript";
-        auto_format = true;
+        auto-format = true;
       }
       {
         name = "typescript";
-        auto_format = true;
+        auto-format = true;
       }
       {
         name = "rust";
-        auto_format = true;
+        auto-format = true;
       }
       {
         name = "java";
-        auto_format = true;
+        auto-format = true;
       }
     ];
   };
