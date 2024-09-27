5b5bb89a8930abb932a06846b6f3832426f21ff9
= <hex.execute@gmail.com>
2024-09-26 21:02:55 -0400

    attempted to setup zathura and LaTex

--- user/pkg/helix/default.nix
+++ user/pkg/helix/default.nix
@@ -1,24 +1,34 @@
-{ self, ... }: {
+{ self, config, pkgs, ... }: {
   # home.sessionVariables.EDITOR = "hx";
 
-  imports = [
-    "${self}/user/pkg/helix/languages.nix"
-  ];
+  imports = [ "${self}/user/pkg/helix/languages.nix" ];
 
-  programs.helix = {
-    enable = true;
-    settings = {
-      theme = "catppuccin_mocha_transparent";
-      editor.cursor-shape = {
-        normal = "block";
-        insert = "bar";
-        select = "underline";
+  home.packages = with pkgs; [ texlab tectonic ];
+
+  programs = {
+    zathura = {
+      enable = true;
+      options = {
+        default_bg = config.theme.colors.surface;
+        default_fg = config.theme.colors.onSurface;
       };
     };
-    themes = {
-      catppuccin_mocha_transparent = {
-        "inherits" = "catppuccin_mocha";
-        "ui.background" = { };
+
+    helix = {
+      enable = true;
+      settings = {
+        theme = "catppuccin_mocha_transparent";
+        editor.cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
+      };
+      themes = {
+        catppuccin_mocha_transparent = {
+          "inherits" = "catppuccin_mocha";
+          "ui.background" = { };
+        };
       };
     };
   };
--- user/pkg/helix/languages.nix
+++ user/pkg/helix/languages.nix
@@ -1,25 +1,58 @@
 { pkgs, ... }: {
-  programs.helix.languages.language = [
-    {
-      name = "nix";
-      auto-format = true;
-      formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-    }
-    {
-      name = "javascript";
-      auto-format = true;
-    }
-    {
-      name = "typescript";
-      auto-format = true;
-    }
-    {
-      name = "rust";
-      auto-format = true;
-    }
-    {
-      name = "java";
-      auto-format = true;
-    }
-  ];
+  programs.helix.languages = {
+    language_server.texlab.config.texlab = {
+      chktex = {
+        on_open_and_save = true;
+        on_edit = true;
+      };
+      forward_search = {
+        executable = "zathura";
+        args = [ "--synctex-forward" "%l:%c:%f" "%p" ];
+      };
+      build = {
+        aux_directory = "build";
+        log_directory = "build";
+        pdf_directory = "build";
+
+        forward_search_after = true;
+        on_save = true;
+
+        executable = "tectonic";
+
+        args = [
+          "-X"
+          "compile"
+          "--synctex"
+          "--keep-logs"
+          "--keep-intermediates"
+          "--outdir=build"
+          "%f"
+        ];
+      };
+    };
+
+    language = [
+      {
+        name = "nix";
+        auto_format = true;
+        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+      }
+      {
+        name = "javascript";
+        auto_format = true;
+      }
+      {
+        name = "typescript";
+        auto_format = true;
+      }
+      {
+        name = "rust";
+        auto_format = true;
+      }
+      {
+        name = "java";
+        auto_format = true;
+      }
+    ];
+  };
 }
