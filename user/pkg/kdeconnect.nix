c86d795e5ce4316b1fda62214bc5d89cf4978e9e
= <hex.execute@gmail.com>
2024-10-13 14:27:10 -0400

    attepmting to disable firewall for kdeconnect

--- user/pkg/kdeconnect.nix
+++ user/pkg/kdeconnect.nix
@@ -1,1 +1,16 @@
-{ services.kdeconnect.enable = true; }
+{
+  networking.firewall = {
+    enable = true;
+    allowedTCPPortRanges = [{
+      from = 1714;
+      to = 1764;
+    } # KDE Connect
+      ];
+    allowedUDPPortRanges = [{
+      from = 1714;
+      to = 1764;
+    } # KDE Connect
+      ];
+  };
+  services.kdeconnect.enable = true;
+}
