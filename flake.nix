05e5ce441618c179f18f6d706e1bb5ac99438b9b
= <hex.execute@gmail.com>
2024-12-02 17:47:12 -0500

    permitted another dotnet

--- flake.nix
+++ flake.nix
@@ -28,8 +28,11 @@
         modules = [
           ./sys/default.nix
           {
-            nixpkgs.config.permittedInsecurePackages =
-              [ "dotnet-runtime-6.0.36" "dotnet-sdk-wrapped-6.0.428" ];
+            nixpkgs.config.permittedInsecurePackages = [
+              "dotnet-runtime-6.0.36"
+              "dotnet-sdk-wrapped-6.0.428"
+              "dotnet-sdk-6.0.428"
+            ];
           }
         ];
       };
