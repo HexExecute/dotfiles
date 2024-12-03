2929abf0df3afbe7a8170c3e79f821fbef35e4d1
= <hex.execute@gmail.com>
2024-12-03 17:07:19 -0500

    updated udev rules

--- sys/service/udev.nix
+++ sys/service/udev.nix
@@ -1,1 +1,9 @@
-{ services.udev.extraRules = ""; }
+{
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", MODE="0666"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6010", MODE="0666"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6011", MODE="0666"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6014", MODE="0666"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6015", MODE="0666"
+  '';
+}
