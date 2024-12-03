5237425c469d4e813ca0bc264149e6d523ac7de6
= <hex.execute@gmail.com>
2024-12-02 19:00:58 -0500

    removed redundant hyprland settings

--- user/pkg/wezterm/default.nix
+++ user/pkg/wezterm/default.nix
@@ -2,21 +2,21 @@
   programs.wezterm = {
     enable = true;
     extraConfig = ''
-      local wezterm = require 'wezterm'
+            local wezterm = require 'wezterm'
 
-      local config = wezterm.config_builder()
+            local config = wezterm.config_builder()
 
-      config.color_scheme = 'Catppuccin Mocha'
-      config.enable_tab_bar = false
-      config.window_background_opacity = 0.8
-	  config.front_end = "WebGpu"
+            config.color_scheme = 'Catppuccin Mocha'
+            config.enable_tab_bar = false
+            config.window_background_opacity = 0.8
+      	    config.front_end = 'WebGpu'
 
-      config.colors = {
-        foreground = '${config.theme.colors.onSurface}',
-        background = '${config.theme.colors.surface}'
-      }
+            config.colors = {
+              foreground = '${config.theme.colors.onSurface}',
+              background = '${config.theme.colors.surface}'
+            }
 
-      return config
+            return config
     '';
 
     # builtins.readFile "${self}/user/pkg/wezterm/config/default.lua";
