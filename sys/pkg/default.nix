cbe434768bf43040c29a5b8bd899808f57d502e8
= <hex.execute@gmail.com>
2024-12-03 17:02:18 -0500

    added usbutils and a udev file

--- sys/pkg/default.nix
+++ sys/pkg/default.nix
@@ -1,5 +1,6 @@
 { self, ... }: {
   imports = [
+    "${self}/sys/pkg/cli.nix"
     "${self}/sys/pkg/ftdi.nix"
     "${self}/sys/pkg/gnome.nix"
     "${self}/sys/pkg/kdeconnect.nix"
--- sys/service/default.nix
+++ sys/service/default.nix
@@ -5,6 +5,7 @@
     "${self}/sys/service/gnome.nix"
     "${self}/sys/service/polkit.nix"
     "${self}/sys/service/upower.nix"
+    "${self}/sys/service/udev.nix"
     "${self}/sys/service/bluetooth.nix"
     "${self}/sys/service/pipewire.nix"
     "${self}/sys/service/tablet.nix"
