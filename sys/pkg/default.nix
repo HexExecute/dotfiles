13469282750031453678d504d16dbb5ed7a0bc4f
= <hex.execute@gmail.com>
2024-12-03 13:08:05 -0500

    added libftdi

--- sys/pkg/default.nix
+++ sys/pkg/default.nix
@@ -1,3 +1,7 @@
 { self, ... }: {
-  imports = [ "${self}/sys/pkg/gnome.nix" "${self}/sys/pkg/kdeconnect.nix" ];
+  imports = [
+    "${self}/sys/pkg/ftdi.nix"
+    "${self}/sys/pkg/gnome.nix"
+    "${self}/sys/pkg/kdeconnect.nix"
+  ];
 }
