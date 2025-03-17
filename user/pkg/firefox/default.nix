ba56c3a82cd20189d5098f06dd40128ba243c792
= <hex.execute@gmail.com>
2025-03-17 15:51:38 -0400

    removed firefox not needed and added a private browser

--- user/pkg/firefox/default.nix
+++ user/pkg/firefox/default.nix
@@ -1,7 +1,8 @@
-{ pkgs, system, inputs, ... }: {
-  programs.firefox = {
-    enable = true;
-    package = pkgs.firefox;
-  };
-  home.packages = [ inputs.zen-browser.packages.x86_64-linux.beta ];
+{ pkgs, inputs, ... }: {
+  # programs.firefox = {
+  #   enable = true;
+  #   package = pkgs.firefox;
+  # };
+  home.packages =
+    [ inputs.zen-browser.packages.x86_64-linux.beta pkgs.tor-browser ];
 }
