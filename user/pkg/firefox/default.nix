aaf17793c8c9f91e68c07240a6f46ce80c2524df
= <hex.execute@gmail.com>
2025-04-17 13:08:08 -0400

    switched to chromium and put on unstable

--- user/pkg/firefox/default.nix
+++ user/pkg/firefox/default.nix
@@ -1,8 +1,8 @@
-{ pkgs, inputs, ... }: {
+{ unstable, inputs, ... }: {
   # programs.firefox = {
   #   enable = true;
   #   package = pkgs.firefox;
   # };
   home.packages =
-    [ inputs.zen-browser.packages.x86_64-linux.beta pkgs.ungoogled-chromium ];
+    [ inputs.zen-browser.packages.x86_64-linux.beta unstable.chromium ];
 }
