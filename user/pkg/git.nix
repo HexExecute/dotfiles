e7215a83ca3cf905263ae8a60c182d9e2bf6c1d4
= <hex.execute@gmail.com>
2025-07-21 19:28:02 -0400

    made sure turtle had dependencies

--- user/pkg/git.nix
+++ user/pkg/git.nix
@@ -1,4 +1,11 @@
 { pkgs, ... }: {
   programs.git.enable = true;
-  home.packages = with pkgs; [ gitui turtle ];
+  home.packages = with pkgs; [
+    gitui
+    turtle
+    nautilus-python
+    python313Packages.pygit2
+    python313Packages.pydbus
+    python313Packages.secretstorage
+  ];
 }
